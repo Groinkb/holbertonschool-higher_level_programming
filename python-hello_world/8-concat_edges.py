eae877ce1b4dc3d41be1ffb0292747b9e62fcb95
Groinkb <benjamin@mossu.ch>
2023-09-26 14:49:16 +0200

    fix

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -3,6 +3,3 @@ str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 word = str[39:67] + str[107:112] + str[:6]
 print(word)
-# str[39:67]: Cette expression extrait une sous-chaîne de caractères de la variable str, commençant à l'indice 39 et se terminant juste avant l'indice 67
-#str[107:112]: Cette partie extrait une autre sous-chaîne de caractères de la variable str, commençant à l'indice 107 inclus et se terminant à l'indice 112 exclu.
-#str[:6]: Cette partie extrait une sous-chaîne de caractères de la variable str, commençant depuis le début de la chaîne (indice 0) jusqu'à l'indice 6 exclu.
