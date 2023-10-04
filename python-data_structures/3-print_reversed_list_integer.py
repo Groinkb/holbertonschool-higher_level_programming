dd72ab5e279295ea84e6e5b192ca9592b8ee08e3
Groinkb <benjamin@mossu.ch>
2023-10-04 14:57:17 +0200

    reversed list

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        my_list = []
+    my_list.reverse()
+    [print("{:d}".format(i)) for i in my_list]
