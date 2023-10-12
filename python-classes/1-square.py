e5b6e9f77ebafb7e522d9c0a6c74138e0ea9e8e5
Groinkb <benjamin@mossu.ch>
2023-10-12 16:42:55 +0200

    fix output

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
 class Square:
-    def __init__(self, size=0):
-        if size < 0:
-            print("Size must be >= 0")
-            # You can raise an exception here if you want to handle it differently
-            # For example, raise ValueError("Size must be >= 0")
-        else:
-            self.__size = size
+    """A class to represent a square."""
 
-# Usage
-my_square = Square(5)
-print(my_square.__dict__.get('_Square__size', 'Invalid size'))  # Accessing private attribute
+    def __init__(self, size):
+        """Initialize the Square object with a given size.
+
+        Args:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
+
+# Example usage
+square = Square(5)
+print(square.__dict__.get('_Square__size'))  # Accessing private attribute
