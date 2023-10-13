93ee98c7ce860c5c89e87dbf12880e07b3bc6149
Groinkb <benjamin@mossu.ch>
2023-10-13 10:15:20 +0200

    square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
+Square = __import__('1-square').Square
 
-class Square:
-    """A class to represent a square."""
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
 
-    def __init__(self, size):
-        """Initialize the Square object with a given size.
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
 
-        Args:
-            size (int): The size of the square.
-
-        """
-        self.__size = size
-
-# Example usage
-square = Square(5)
-print(square.__dict__.get('_Square__size'))  # Accessing private attribute
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
