59bb6eafb29daf103057c2919535056693e96d95
Groinkb <benjamin@mossu.ch>
2023-10-20 21:12:13 +0200

    doc

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""function that load list from file then add args in list and save it"""
+"""function that load list from file then add args in list and save itt"""
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
