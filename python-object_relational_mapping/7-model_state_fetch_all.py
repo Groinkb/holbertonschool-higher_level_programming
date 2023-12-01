60b31431321395b576d316281c48742c6c776a86
Groinkb <benjamin@mossu.ch>
2023-12-01 10:24:02 +0100

    add

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-""" lists all State objects from the databasee hbtn_0e_6_usa """
-
-import sys
+"""a script that lists all State objects"""
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 if __name__ == "__main__":
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    engine = create_engine('mysql://{}:{}@localhost:3306/{}'
-                           .format(
-                               mysql_username,
-                               mysql_password,
-                               database_name
-                               ), pool_pre_ping=True)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+    """connecting to database"""
+    con = "mysql://{}:{}@localhost:{}/{}".format(
+        argv[1], argv[2], 3306, argv[3])
+    engine = create_engine(con)
+    """quering data (select * from states order by id;)"""
+    make_session = sessionmaker(bind=engine)
+    session = make_session()
+    res = session.query(State).order_by(State.id).all()
+    """printing instances"""
+    for i in res:
+        print("{}: {}".format(i.id, i.name))
