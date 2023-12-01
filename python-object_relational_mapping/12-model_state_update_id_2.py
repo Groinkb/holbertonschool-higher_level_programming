c660cad452081a328c4701e22e3e0326757accf7
Groinkb <benjamin@mossu.ch>
2023-12-01 10:25:31 +0100

    add

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
-""" adds the State object “Louisiana”
-to the database hbtn_0e_6_usa """
-
-import sys
+"""a script that changes the name of
+a State object from the database"""
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
 if __name__ == "__main__":
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    engine = create_engine('mysql://{}:{}@localhost:3306/{}'
-                           .format(
-                               mysql_username,
-                               mysql_password,
-                               database_name
-                               ), pool_pre_ping=True)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    state_to_update = session.query(State).filter(State.id == 2).first()
-    if state_to_update:
-        state_to_update.name = "New Mexico"
-        session.commit()
-    else:
-        print("State with id=2 not found")
-
-    print("Updated State: {} - {}".format(
-        state_to_update.id, state_to_update.name))
+    """connecting to database"""
+    con = "mysql://{}:{}@localhost:{}/{}".format(
+        argv[1], argv[2], 3306, argv[3])
+    engine = create_engine(con)
+    make_session = sessionmaker(bind=engine)
+    session = make_session()
+    """update state object"""
+    new = session.query(State).filter(State.id == 2).first()
+    new.name = "New Mexico"
+    session.commit()
